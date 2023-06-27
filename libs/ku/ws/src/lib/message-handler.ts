7b6eaf0cd677ec233d5080ded1475d2c859cac2b
nik <nikita.nodejs@gmail.com>
2023-06-27 02:01:41 +0200

    refactor

--- libs/ku/ws/src/lib/message-handler.ts
+++ libs/ku/ws/src/lib/message-handler.ts
@@ -1,14 +1,18 @@
-import {Channel, KuWs, TechChannel} from "@hft/types/ku";
+import { Channel, KuWs } from '@hft/types/ku';
 
 type NoSubjectHandler = (...args: any[]) => void;
-export class MessageHandler implements Record<KuWs[Channel]['SUB']['PAYLOAD']['subject'], any>{
+export class MessageHandler
+  implements Record<KuWs[Channel]['SUB']['PAYLOAD']['subject'], any>
+{
   private undefined: NoSubjectHandler;
 
-  private handlers = new Map<Channel, ((jData: KuWs[Channel]['SUB']['PAYLOAD']) => void)[]>([
-    ['LEVEL_2', []]
-  ] satisfies [
-    [Channel, ((jData: KuWs['LEVEL_2']['SUB']['PAYLOAD']) => void)[]]
-  ]);
+  private handlers = new Map<
+    Channel,
+    ((jData: KuWs[Channel]['SUB']['PAYLOAD']) => void)[]
+  >([
+    ['LEVEL_2', []],
+    ['ACCOUNT_BALANCE', []],
+  ] satisfies [[Channel, ((jData: KuWs['LEVEL_2']['SUB']['PAYLOAD']) => void)[]], [Channel, ((jData: KuWs['ACCOUNT_BALANCE']['SUB']['PAYLOAD']) => void)[]]]);
 
   /**
    * @deprecated
@@ -16,11 +20,14 @@ export class MessageHandler implements Record<KuWs[Channel]['SUB']['PAYLOAD']['s
    * It is not deprecated, BUT!
    * You should get instance of it only from SubscriptionManager.getMessageHandler()
    */
-  constructor(private noSubjectHandler: NoSubjectHandler) {
+  constructor(noSubjectHandler: NoSubjectHandler) {
     this.undefined = noSubjectHandler;
   }
 
-  public addHandler<T extends Channel>(channel: T, cb: (jData: KuWs[T]['SUB']['PAYLOAD']) => void) {
+  public addHandler<T extends Channel>(
+    channel: T,
+    cb: (jData: KuWs[T]['SUB']['PAYLOAD']) => void
+  ) {
     this.handlers.get(channel)!.push(cb);
 
     (cb as any).cleaner = Date.now();
@@ -31,9 +38,11 @@ export class MessageHandler implements Record<KuWs[Channel]['SUB']['PAYLOAD']['s
   public rmHandler(channel: Channel, cleaner: number) {
     const channelHandlers = this.handlers.get(channel)!;
 
-    const index = channelHandlers.findIndex((cb) => (cb as any).cleaner === cleaner);
+    const index = channelHandlers.findIndex(
+      (cb) => (cb as any).cleaner === cleaner
+    );
 
-    if(index !== -1) {
+    if (index !== -1) {
       channelHandlers.splice(index, 1);
     }
   }
@@ -45,7 +54,22 @@ export class MessageHandler implements Record<KuWs[Channel]['SUB']['PAYLOAD']['s
    * You should not directly call this method!
    * But dynamically, from message.subject - OK.
    */
-  "trade.l2update"(jData: KuWs['LEVEL_2']['SUB']['PAYLOAD']) {
-    this.handlers.get('LEVEL_2' satisfies Channel)!.forEach((cb) => void cb(jData));
+  'trade.l2update'(jData: KuWs['LEVEL_2']['SUB']['PAYLOAD']) {
+    this.handlers
+      .get('LEVEL_2' satisfies Channel)!
+      .forEach((cb) => void cb(jData));
+  }
+
+  /**
+   * @deprecated
+   *
+   * It is not deprecated, BUT!
+   * You should not directly call this method!
+   * But dynamically, from message.subject - OK.
+   */
+  'account.balance'(jData: KuWs['ACCOUNT_BALANCE']['SUB']['PAYLOAD']) {
+    this.handlers
+      .get('ACCOUNT_BALANCE' satisfies Channel)!
+      .forEach((cb) => void cb(jData));
   }
 }
