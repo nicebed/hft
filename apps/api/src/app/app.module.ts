0b096d973bd48577ad6bed169c3c14b822adc159
nik <nikita.nodejs@gmail.com>
2023-07-13 03:33:03 +0200

    developing gql-fabric

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,21 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-
-
+import { GraphQLModule } from '@nestjs/graphql';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory() {
+        return {
+          subscriptions: {
+            'graphql-ws': true,
+          },
+          playground: false,
+          ...(process.env.NODE_ENV !== 'production' && { plugins: [ApolloServerPluginLandingPageLocalDefault()] }),
+        };
+      },
+    }),
+  ],
 })
 export class AppModule {}
