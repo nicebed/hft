a01b81f2158484038ce9fee9df2ed8ffd5dd102f
nik <nikita.nodejs@gmail.com>
2023-07-14 07:35:43 +0200

    apps/app/api/main.ts
    replace default code

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,15 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
+  const logger = new Logger('apps/api::bootstrap');
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
+
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  logger.debug(process.env.NODE_ENV === 'production' ? await app.getUrl() : `http://localhost:${port}`);
 }
 
 bootstrap();
