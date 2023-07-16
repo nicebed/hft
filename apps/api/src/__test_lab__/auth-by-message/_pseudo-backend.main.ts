e623285f3bfb8bff420a9f22c2499d36985a90a2
nik <nikita.nodejs@gmail.com>
2023-07-16 13:55:40 +0200

    no errors

--- apps/api/src/__test_lab__/auth-by-message/_pseudo-backend.main.ts
+++ apps/api/src/__test_lab__/auth-by-message/_pseudo-backend.main.ts
@@ -3,12 +3,13 @@ import { NestFactory } from "@nestjs/core";
 import { GraphQLModule, Resolver } from "@nestjs/graphql";
 import { _TestConfig } from "./_test-config.provider";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { join } from "path";
 
 
 export async function _bootPseudoBackend() {
   const app = await NestFactory.create(_PseudoBackend, { logger: false });
 
-  app.listen(_TestConfig.app.port);
+  await app.listen(_TestConfig.app.port);
 
   return app.close;
 }
@@ -21,12 +22,13 @@ class _PseudoResolver {
 
 
 @Module({
+  providers: [_PseudoResolver],
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     subscriptions: {
       'graphql-ws': true,
     },
-    typePaths: ['./gql/*.graphql'],
+    typePaths: [join(__dirname, './gql/*.graphql')],
     playground: false,
   })]
 })
--- apps/api/src/__test_lab__/auth-by-message/index.spec.ts
+++ apps/api/src/__test_lab__/auth-by-message/index.spec.ts
@@ -1,7 +1,19 @@
+import { _bootPseudoBackend } from './_pseudo-backend.main';
+
 describe(`Auth flow with some messenger. (gql-subscriptions)`, () => {
-  it('should work', (done) => {
+  let closeApp: () => Promise<void>;
+
+  beforeAll(async () => {
+    closeApp = await _bootPseudoBackend();
+  });
+
+  afterAll(async () => {
+    closeApp && (await closeApp());
+  });
+
+  it('should work | authByMessage0', (done) => {
     const test = async () => {
-      // TODO
+      expect('should').not.toBe('errors');
 
       done();
     };
