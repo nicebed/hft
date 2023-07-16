b4d755ca11457d884c2705d7c24ec262d2d4933b
nik <nikita.nodejs@gmail.com>
2023-07-16 12:40:30 +0200

    prepare imagine auth flow using test

---
+++ apps/api/src/__test_lab__/auth-by-message/index.spec.ts
@@ -0,0 +1,11 @@
+describe(`Auth flow with some messenger. (gql-subscriptions)`, () => {
+  it('should work', (done) => {
+    const test = async () => {
+      // TODO
+
+      done();
+    };
+
+    test();
+  });
+});
